0cb96bbf8518118c60d309b9f435085673339cd2
rosierk <Kirill.Rosier@etu.univ-grenoble-alpes.fr>
2025-11-04 15:17:02 +0100

    💄 modification de la page d'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { RoomGrid } from "@/components/room-grid";
+import EventList from "@/components/events-list";
 import * as React from "react";
 
 export default function Home() {
@@ -6,10 +6,10 @@ export default function Home() {
     <div className="min-h-screen bg-background">
       <main className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-8">
-          Salles Musique à proximité de Lyon, FR
+          Commencez votre aventure!
         </h1>
-        <React.Suspense fallback={<div>Chargement des salles...</div>}>
-          <RoomGrid />
+        <React.Suspense fallback={<div>Chargement des événements...</div>}>
+          <EventList/>
         </React.Suspense>
       </main>
     </div>
